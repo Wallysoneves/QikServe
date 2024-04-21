d105c5454f8dac4c2eaa7efc3f43e6a0d80febdc
wallysoneves <wallysoneves.moraes@gmail.com>
2024-04-21 11:38:21 -0300

    Criação da tratativa de erro personalizada

---
+++ src/main/java/com/br/qikserveteste/infrastructure/exception/HandlerException.java
@@ -0,0 +1,26 @@
+package com.br.qikserveteste.infrastructure.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class HandlerException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(QikServeException.class)
+    public ResponseEntity<ErroModel> handleQikServeException(QikServeException ex) {
+        HttpStatus status = ex.getStatus();
+
+        ErroModel apiErrorModel = ErroModel.builder()
+                .status(status.value())
+                .erro(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(apiErrorModel, status);
+    }
+}
