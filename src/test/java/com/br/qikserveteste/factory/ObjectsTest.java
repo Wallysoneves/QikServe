6130406a3dd8b239ad9a45f3c89ba0ef805ef291
wallysoneves <wallysoneves.moraes@gmail.com>
2024-04-22 02:01:42 -0300

    Alterando o nome da class de teste para o singular

--- src/test/java/com/br/qikserveteste/factory/ObjectsTest.java
+++ src/test/java/com/br/qikserveteste/factory/ObjectsTest.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ObjectsTests {
+public class ObjectsTest {
 
     /*-----------------------------------------ORDER WITH OUT PROMOTION-----------------------------------------*/
 
