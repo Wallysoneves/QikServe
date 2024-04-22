1b8f51b0dbc854312477598c11326fe800190904
wallysoneves <wallysoneves.moraes@gmail.com>
2024-04-21 23:13:58 -0300

    tratativa para não dar mais nullpointer, handlerException

--- src/main/java/com/br/qikserveteste/infrastructure/exception/HandlerException.java
+++ src/main/java/com/br/qikserveteste/infrastructure/exception/HandlerException.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 public class HandlerException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(QikServeException.class)
     public ResponseEntity<ErroModel> handleQikServeException(QikServeException ex) {
-        HttpStatus status = ex.getStatus();
+        HttpStatus status = Objects.nonNull(ex.getStatus()) ? ex.getStatus() : HttpStatus.BAD_REQUEST;
 
         ErroModel apiErrorModel = ErroModel.builder()
                 .status(status.value())
